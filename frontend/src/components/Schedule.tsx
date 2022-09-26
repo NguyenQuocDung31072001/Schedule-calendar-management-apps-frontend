fe9f0d89760697d54b46425bec69c8d59c73c6c9
dungnguyen317 <dungnguyen310701@gmail.com>
2022-09-27 00:30:03 +0700

    change library

--- frontend/src/components/Schedule.tsx
+++ frontend/src/components/Schedule.tsx
@@ -1,51 +1,205 @@
-import React from "react";
-import Scheduler, { Resource } from "devextreme-react/scheduler";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import { EditingState } from '@devexpress/dx-react-scheduler';
+import {
+  Appointments,
+  Scheduler,
+  WeekView,
+  DragDropProvider,
+  CurrentTimeIndicator,
+  EditRecurrenceMenu,
+} from '@devexpress/dx-react-scheduler-material-ui';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import InputAdornment from '@mui/material/InputAdornment';
 
-import { subjectData, taskData, data } from "../data";
+import {appointments} from '../demo-data/appointments';
 
-const currentDate = new Date(2021, 1, 2);
-const groups = ["priority"];
-const views: any = [
-  "agenda",
-  "month",
-  "week",
-  "day",
-  //   { type: "day", intervalCount: 7, cellDuration: 15 },
-];
-const Schedule = () => {
-  const [dataSchedule, setDataSchedule] = React.useState(data);
-  return (
-    <div>
-      <Scheduler
-        timeZone="Etc/GMT+7"
-        id="scheduler"
-        dataSource={dataSchedule}
-        views={views}
-        defaultCurrentView="timelineMonth"
-        defaultCurrentDate={currentDate}
-        groups={groups}
-        cellDuration={60}
-        firstDayOfWeek={0}
-        height={600}
-        startDayHour={1}
-        editing={true}
-      >
-        <Resource
-          fieldExpr="subject"
-          allowMultiple={true}
-          dataSource={subjectData}
-          label="Subject"
-          useColorAsDefault={true}
-        />
-        <Resource
-          fieldExpr="task"
-          allowMultiple={true}
-          dataSource={taskData}
-          label="Task"
-        />
-      </Scheduler>
-    </div>
-  );
+const PREFIX = 'Demo';
+
+const classes = {
+  checkBoxContainer: `${PREFIX}-checkBoxContainer`,
+  textField: `${PREFIX}-textField`,
 };
+// #FOLD_BLOCK
+const StyledGrid = styled(Grid)(({ theme: { spacing } }) => ({
+  [`&.${classes.checkBoxContainer}`]: {
+    paddingTop: spacing(1),
+    paddingBottom: spacing(1),
+    paddingLeft: spacing(4),
+  },
+}));
+// #FOLD_BLOCK
+const StyledTextField = styled(TextField)(({ theme: { spacing } }) => ({
+  [`&.${classes.textField}`]: {
+    marginRight: spacing(4),
+    marginLeft: spacing(1),
+    width: '120px',
+  },
+}));
+// #FOLD_BLOCK
+const ShadeCellsCheckBox = (shadePreviousCells:any, handleChange:any) => (
+  <FormControlLabel
+    control={(
+      <Checkbox
+        checked={shadePreviousCells}
+        onChange={() => handleChange('shadePreviousCells')}
+        color="primary"
+      />
+    )}
+    label="Shade previous cells"
+  />
+);
 
-export default Schedule;
+// #FOLD_BLOCK
+const ShadePreviousAppointmentsCheckBox = (shadePreviousAppointments:any, handleChange:any) => (
+  <FormControlLabel
+    control={(
+      <Checkbox
+        checked={shadePreviousAppointments}
+        onChange={() => handleChange('shadePreviousAppointments')}
+        color="primary"
+      />
+    )}
+    label="Shade previous appointments"
+  />
+);
+// #FOLD_BLOCK
+const CheckBoxContainer = ((
+  shadePreviousCells:any, shadePreviousAppointments:any, handleCheckboxChange:any,
+  // #FOLD_BLOCK
+) => (
+  <StyledGrid item container direction="column" className={classes.checkBoxContainer} xs={6}>
+    <ShadeCellsCheckBox
+      shadePreviousCells={shadePreviousCells}
+      handleChange={handleCheckboxChange}
+    />
+    <ShadePreviousAppointmentsCheckBox
+      shadePreviousAppointments={shadePreviousAppointments}
+      handleChange={handleCheckboxChange}
+    />
+  </StyledGrid>
+));
+
+// #FOLD_BLOCK
+const UpdateIntervalBox = ((
+  updateInterval:any, onValueChange:any,
+  // #FOLD_BLOCK
+) => (
+  <Grid item container xs={6} alignItems="center" justifyContent="flex-end">
+    <Typography>
+      Update every:
+    </Typography>
+    <StyledTextField
+      className={classes.textField}
+      variant="outlined"
+      onChange={event => onValueChange(event.target.value)}
+      value={updateInterval / 1000}
+      type="number"
+      InputProps={{
+        endAdornment: <InputAdornment position="end">s</InputAdornment>,
+      }}
+    />
+  </Grid>
+));
+
+export default function App(){
+  const [state,setState]=React.useState({
+    data: appointments,
+    shadePreviousCells: true,
+    shadePreviousAppointments: true,
+    updateInterval: 10000,
+  })
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     data: appointments,
+  //     shadePreviousCells: true,
+  //     shadePreviousAppointments: true,
+  //     updateInterval: 10000,
+  //   };
+
+  //   this.onCommitChanges = this.commitChanges.bind(this);
+  //   this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+  //   this.handleUpdateIntervalChange = (nextValue) => {
+  //     this.setState({
+  //       updateInterval: nextValue * 1000,
+  //     });
+  //   };
+  // }
+function handleUpdateIntervalChange(nextValue:any){
+ setState({
+    updateInterval: nextValue * 1000,
+  });
+};
+  function handleCheckboxChange(stateField) {
+    const { [stateField]: fieldToChange } = this.state;
+    this.setState({
+      [stateField]: !fieldToChange,
+    });
+  }
+
+  function onCommitChanges({ added, changed, deleted }) {
+    this.setState((state) => {
+      let { data } = state;
+      if (added) {
+        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        data = [...data, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        data = data.map(appointment => (
+          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+      }
+      if (deleted !== undefined) {
+        data = data.filter(appointment => appointment.id !== deleted);
+      }
+      return { data };
+    });
+  }
+    return (
+      <React.Fragment>
+        <Grid container>
+          <CheckBoxContainer
+            shadePreviousCells={shadePreviousCells}
+            shadePreviousAppointments={shadePreviousAppointments}
+            handleCheckboxChange={handleCheckboxChange}
+          />
+          <UpdateIntervalBox
+            updateInterval={updateInterval}
+            onValueChange={handleUpdateIntervalChange}
+          />
+        </Grid>
+
+        <Paper>
+          <Scheduler
+            data={data}
+            height={660}
+          >
+            <EditingState
+              onCommitChanges={onCommitChanges}
+            />
+            <EditRecurrenceMenu />
+            <WeekView
+              startDayHour={9}
+              endDayHour={19}
+            />
+
+            <Appointments />
+            <DragDropProvider />
+
+            <CurrentTimeIndicator
+              shadePreviousCells={shadePreviousCells}
+              shadePreviousAppointments={shadePreviousAppointments}
+              updateInterval={updateInterval}
+            />
+          </Scheduler>
+        </Paper>
+      </React.Fragment>
+    );
+  
+}
